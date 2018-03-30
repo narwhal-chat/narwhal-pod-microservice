97d9e278d3877e263355743a9fca22b972b95b77
Rory Well <rory.e.well@gmail.com>
2018-03-30 02:21:08 -0500

    Rearrange order of getAllPods query

--- db/queries.js
+++ db/queries.js
@@ -19,6 +19,19 @@ const pods = {
     }
   },
 
+  // Return all pods
+  getAllPods: async () => {
+    try {
+      const pods = await db.any('SELECT p.id, p.reference_name, p.display_name, p.description, p.avatar, p.pod_category_id, pc.name pod_category_name, p.author_id, p.create_date, p.is_deleted, p.delete_date, uc.user_count ' +
+          'FROM pod p ' +
+          'INNER JOIN pod_category pc ON p.pod_category_id = pc.id ' +
+          'INNER JOIN (SELECT count(*) user_count, p.id FROM pod p INNER JOIN pod_user pu ON p.id = pu.pod_id GROUP BY p.id) uc ON p.id = uc.id;');
+      return pods;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
   // Create a pod
   createPod: (newPod) => {
     const response = db.tx(async t => {
@@ -56,19 +69,6 @@ const pods = {
       .catch(e => {
         console.log(e);
       });
-  },
-
-  // Return all pods
-  getAllPods: async () => {
-    try {
-      const pods = await db.any('SELECT p.id, p.reference_name, p.display_name, p.description, p.avatar, p.pod_category_id, pc.name pod_category_name, p.author_id, p.create_date, p.is_deleted, p.delete_date, uc.user_count ' +
-          'FROM pod p ' +
-          'INNER JOIN pod_category pc ON p.pod_category_id = pc.id ' +
-          'INNER JOIN (SELECT count(*) user_count, p.id FROM pod p INNER JOIN pod_user pu ON p.id = pu.pod_id GROUP BY p.id) uc ON p.id = uc.id;');
-      return pods;
-    } catch (e) {
-      console.log(e);
-    }
   },
 
   // Add user to a pod
